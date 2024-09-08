e50a8d862b883948a72e96aa78efe6a9847f8e05
tcgcollection <David.cohin.ac@gmail.com>
2024-09-08 17:50:57 +0200

    Intégration du code de TCG Marketplace dans le projet

--- public/script.js
+++ public/script.js
@@ -1,1 +1,39 @@
-console.log('Bienvenue sur mon site Vercel !');
+new Vue({
+  el: '#app',
+  data: {
+    cards: [],
+    displayedCards: [],
+    cart: [],
+    cartOpen: false,
+    filters: {
+      search: '',
+      minPrice: null,
+      maxPrice: null,
+      conditions: [],
+      languages: []
+    },
+    sortBy: 'name_asc',
+    conditions: ['Mint', 'Near Mint', 'Excellent', 'Good', 'Played'],
+    languages: ['English', 'Japanese', 'German', 'French', 'Italian', 'Spanish']
+  },
+  methods: {
+    fetchCards() {
+      // Logic for fetching cards
+    },
+    applyFilters() {
+      // Logic for filtering and sorting cards
+    },
+    addToCart(card) {
+      this.cart.push(card);
+    },
+    removeFromCart(index) {
+      this.cart.splice(index, 1);
+    },
+    toggleCart() {
+      this.cartOpen = !this.cartOpen;
+    }
+  },
+  mounted() {
+    this.fetchCards();
+  }
+});
